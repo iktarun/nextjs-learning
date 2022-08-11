7f03eed868bcb1e2e7aef91cab03601b43058646
Tarun Kumar <tarunkumar2215@gmail.com>
2022-08-11 16:28:57 +0530

    Next.js Tutorial - 11 - Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Placing Your Order");
+    router.push("/product");
+  };
+
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>{" "}
+      <Link href="/product">
+        <a>Product</a>
+      </Link>
+      <button onClick={handleClick}>Place Order</button>
+    </div>
+  );
 }
 
 export default HomePage;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,5 +1,31 @@
-function Product() {
-  return <div>Product Page</div>;
+import Link from "next/link";
+
+function ProductList() {
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <Link href="/">
+        <a>Home Page</a>
+      </Link>
+      <ul>
+        <li>
+          <Link href="/product/1">
+            <a>Product 1</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/product/2">
+            <a>Product 2</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/product/3">
+            <a>Product 3</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Product;
+export default ProductList;
