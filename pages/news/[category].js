26887c1dca19558f6356058cd185fd9ba71258ad
Tarun Kumar <tarunkumar2215@gmail.com>
2022-08-22 19:09:00 +0530

    Next.js Tutorial - 36 - Client-side Data Fetching

--- pages/news/[category].js
+++ pages/news/[category].js
@@ -1,3 +1,5 @@
+import { serialize } from "cookie";
+
 function ArticleListByCategory({ articles, category }) {
   return (
     <>
@@ -18,13 +20,18 @@ function ArticleListByCategory({ articles, category }) {
 export default ArticleListByCategory;
 
 export async function getServerSideProps(context) {
-  const { params } = context;
+  const { params, req, res, query } = context;
+  console.log(req.headers.cookie, query);
+  //Setting cookies directly was giving some errors
+  // res.setHeader('Set-Cookie', ['name'='Tarun']);
+  res.setHeader("Set-Cookie", serialize("name", "Tarun"));
   const { category } = params;
   const response = await fetch(
     `http://localhost:4000/news?category=${category}`
   );
 
   const data = await response.json();
+  console.log(`Pre-Rendering New Articles for Category ${category}`);
 
   return {
     props: {
