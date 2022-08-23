7bf11d968a5efe4202162a74e8ada2dc2a21f248
Tarun Kumar <tarunkumar2215@gmail.com>
2022-08-23 23:38:32 +0530

    Next.js Tutorial - 43 - API POST Request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,14 +2,39 @@ import React from "react";
 
 function CommentPage() {
   const [comments, setComments] = React.useState([]);
+  const [comment, setComment] = React.useState("");
 
   const fetchComments = async () => {
     const response = await fetch("http://localhost:3000/api/comments");
     const data = await response.json();
     setComments(data);
   };
+  const submitComment = async () => {
+    const response = await fetch("http://localhost:3000/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    setComments(data);
+  };
+
   return (
     <>
+      <div>
+        <input
+          type="text"
+          value={comment}
+          onChange={(e) => {
+            setComment(e.target.value);
+          }}
+        />
+        <button onClick={submitComment}>Submit Comment</button>
+      </div>
+      <hr />
       <button onClick={fetchComments}>Fetch Comment</button>
       {comments.map((comment) => {
         return (
